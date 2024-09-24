4e819beed2ddd0cef99c795f043205f5d81d9a76
ivanontech <67839160+ivanontech@users.noreply.github.com>
2024-09-23 22:43:54 -0400

    Add files via upload

---
+++ track_balances.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+LOG_FILE="/root/track_balances.log"
+
+# Navigate to the correct directory
+cd /root/ceremonyclient/node || exit 1  # Exit if directory change fails
+
+# Fetch balance and peer ID
+PEER_ID=$(./node-1.4.21.1-linux-amd64 -peer-id | grep "Peer ID" | awk '{print $3}')
+BALANCE=$(./node-1.4.21.1-linux-amd64 -balance | grep "Unclaimed balance" | awk '{print $3}')
+
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Log peer ID and balance
+echo "[$DATE] Peer ID: $PEER_ID, Balance: $BALANCE" >> $LOG_FILE
+
+# Send the data to the Flask server
+curl -X POST -H "Content-Type: application/json" \
+-d "{\"peer_id\":\"$PEER_ID\", \"balance\":\"$BALANCE\", \"timestamp\":\"$DATE\"}" \
+http://0.0.0.0:5000/update_balance >> $LOG_FILE 2>&1
