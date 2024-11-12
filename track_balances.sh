fdeed8e6b70c74f6d91bb3c91d0454ff232d1571
ivanontech <67839160+ivanontech@users.noreply.github.com>
2024-11-12 14:13:37 -0500

    Update track_balances.sh

--- track_balances.sh
+++ track_balances.sh
@@ -5,16 +5,22 @@ LOG_FILE="/root/track_balances.log"
 # Navigate to the correct directory
 cd /root/ceremonyclient/node || exit 1  # Exit if directory change fails
 
-# Fetch balance and peer ID
-PEER_ID=$(./node-2.0.3.3-linux-amd64 -peer-id | grep "Peer ID" | awk '{print $3}')
-BALANCE=$(./node-2.0.3.3-linux-amd64 -balance | grep "Unclaimed balance" | awk '{print $3}')
+# Run the command to get node info
+NODE_INFO_OUTPUT=$(./node-2.0.3.3-linux-amd64 -node-info)
+
+# Extract Peer ID, Version, Max Frame, Prover Ring, Seniority, and Owned Balance
+PEER_ID=$(echo "$NODE_INFO_OUTPUT" | grep "Peer ID" | awk '{print $3}')
+VERSION=$(echo "$NODE_INFO_OUTPUT" | grep "Version" | awk '{print $2}')
+MAX_FRAME=$(echo "$NODE_INFO_OUTPUT" | grep "Max Frame" | awk '{print $3}')
+PROVER_RING=$(echo "$NODE_INFO_OUTPUT" | grep "Prover Ring" | awk '{print $3}')
+SENIORITY=$(echo "$NODE_INFO_OUTPUT" | grep "Seniority" | awk '{print $2}')
+BALANCE=$(echo "$NODE_INFO_OUTPUT" | grep "Owned balance" | awk '{print $3}')
 
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-# Log peer ID and balance
-echo "[$DATE] Peer ID: $PEER_ID, Balance: $BALANCE" >> $LOG_FILE
+# Log all extracted information
+echo "[$DATE] Peer ID: $PEER_ID, Version: $VERSION, Max Frame: $MAX_FRAME, Prover Ring: $PROVER_RING, Seniority: $SENIO>
 
 # Send the data to the Flask server
 curl -X POST -H "Content-Type: application/json" \
--d "{\"peer_id\":\"$PEER_ID\", \"balance\":\"$BALANCE\", \"timestamp\":\"$DATE\"}" \
-http://0.0.0.0:5000/update_balance >> $LOG_FILE 2>&1
+-d "{\"peer_id\":\"$PEER_ID\", \"version\":\"$VERSION\", \"max_frame\":\"$MAX_FRAME\", \"prover_ring\":\"$PROVER_RING\">http://0.0.0.0:5000/update_balance >> $LOG_FILE 2>&1
